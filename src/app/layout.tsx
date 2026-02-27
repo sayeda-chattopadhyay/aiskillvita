3e6e8d77f14343a3ed5738209ba200688210703e
Sayeda Chattopadhyay <sayeda.b@gmail.com>
2026-02-27 15:54:52 +0100

    feat: add Navbar to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Navbar } from "@/components/layout/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Navbar />
+        <div className="pt-14">{children}</div>
       </body>
     </html>
   );
